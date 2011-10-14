6835253cd927fcc093147cda94f5f64631c344ea
Ricardo Franco <ricardo.krieg@gmail.com>
2011-10-13 22:02:13 -0300

    filling inputs a simpler way

--- perceptron.js
+++ perceptron.js
@@ -1,7 +1,5 @@
-var n_lines = 1;
-var n_ins = 3;
 var inputs = null;
-var w = new Array(-3.0839, 1.560212, 2.475722, -0.735356);
+var w = null;
 
 function load_w(){
 	w = new Array(4);
@@ -10,21 +8,10 @@ function load_w(){
 }
 
 function load_inputs(){
-	temp_line_inputs = $('#inputs').val().split('\n');
-	temp_inputs = temp_line_inputs[0].split(';');
-
-	line_inputs = temp_line_inputs.length;
-	n_inputs = temp_inputs.length;
-
-	inputs = new Array(line_inputs);
-	for (i=0;i<line_inputs;++i){
-		inputs[i] = new Array(n_inputs);
+	inputs = $('#inputs').val().split('\n');
+	for (i=0; i<inputs.length; ++i){
+		inputs[i] = inputs[i].split(';');
 	}
-	for (i=0; i<inputs.length; i++){
-		inputs[i] = temp_line_inputs[i].split(';');
-	}
-
-	console.log(inputs);
 }
 
 function run(){
